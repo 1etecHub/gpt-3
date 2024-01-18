7a325a64e5674be7523c62c47f3c6d8a69d6bcb5
Ebenezer <ebenirene5@gmail.com>
2024-01-18 17:57:25 +0100

    initial commit with the root folders

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div>
+      Article
+    </div>
+  )
+}
+
+export default Header
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './possibility.css';
+
+const Possibility = () => {
+  return (
+    <div>
+      Possiblility
+    </div>
+  )
+}
+
+export default Possibility;
